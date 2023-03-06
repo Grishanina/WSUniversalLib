244248981233c5728e972d81c4db4a2ef2736135
ГришанинаКИ <@211-04>
2023-03-06 09:52:23 +0300

    Добавьте файлы проекта.

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using Microsoft.Analytics.UnitTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using WSUniversalLib;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        //Низкие тесты
+        //Проверяет корректность метода (что kode состоит из 17 правильных букв)
+        [TestMethod]
+        public void CheckKODE_IsTrue()
+        {
+            string kode = "JHMCM56557C404453";
+            bool actual = Calculation.CheckKODE(kode);
+            Assert.IsTrue(actual);
+        }
+
+        //Проверяет корректность метода (что kode состоит из 17 букв, но некоторые буквы не являются правильными)
+        [TestMethod]
+        public void CheckKODE_IsFalse()
+        {
+            string kode = "OHMCM56557C404453";
+            bool actual = Calculation.CheckKODE(kode);
+            Assert.IsFalse(actual);
+        }
+        //Проверяет корректность метода (что kode состоит из 17 букв, хотя введем всего лишь 16)
+        [TestMethod]
+        public void CheckKODE_IsFalse_16bykv()
+        {
+            string kode = "HMCM56557C404453";
+            bool actual = Calculation.CheckKODE(kode);
+            Assert.IsFalse(actual);
+        }
+        //Проверяет корректность метода (Выведет Африку)
+        [TestMethod]
+        public void GetKODECountry_Africa()
+        {
+            string kode = "JHMCM5655AAAA4453";
+            string conutry = "Африка";
+            string actual = Calculation.GetKODECountry(kode);
+            Assert.AreEqual(conutry, actual);
+        }
+        //Проверяет корректность метода (Выведет Азию)
+        [TestMethod]
+        public void GetKODECountry_Azia()
+        {
+            string kode = "JHMCM5655AKKA4453";
+            string conutry = "Азия";
+            string actual = Calculation.GetKODECountry(kode);
+            Assert.AreEqual(conutry, actual);
+        }
+        //Проверяет корректность метода (Выведет Европа)
+        [TestMethod]
+        public void GetKODECountry_Evropa()
+        {
+            string kode = "JHMCM5655AKTA4453";
+            string conutry = "Европа";
+            string actual = Calculation.GetKODECountry(kode);
+            Assert.AreEqual(conutry, actual);
+        }
+        //Проверяет корректность метода (Выведет Северная Америка)
+        [TestMethod]
+        public void GetKODECountry_Severnay_America()
+        {
+            string kode = "JHMCM5655AK1A4453";
+            string conutry = "Северная Америка";
+            string actual = Calculation.GetKODECountry(kode);
+            Assert.AreEqual(conutry, actual);
+        }
+        //Проверяет корректность метода (Выведет Океания)
+        [TestMethod]
+        public void GetKODECountry_Okeania()
+        {
+            string kode = "JHMCM5655AK6A4453";
+            string conutry = "Океания";
+            string actual = Calculation.GetKODECountry(kode);
+            Assert.AreEqual(conutry, actual);
+        }
+        //Проверяет корректность метода (Выведет Южная Америка)
+        [TestMethod]
+        public void GetKODECountry_South_America()
+        {
+            string kode = "JHMCM5655AK9A4453";
+            string conutry = "Южная Америка";
+            string actual = Calculation.GetKODECountry(kode);
+            Assert.AreEqual(conutry, actual);
+        }
+
+        //Проверяет корректность метода (Выдает данные)
+        [TestMethod]
+        public void GetKODECountry_IsNotNull()
+        {
+            string kode = "JHMCM5655AK9A44";
+            string actual = Calculation.GetKODECountry(kode);
+            Assert.IsNotNull(actual);
+        }
+
+        //Сложные тесты
+        //Проверяет какой тип данных выдаст
+        [TestMethod]
+        public void GetKODECountry_Type()
+        {
+            string kode = "JHMCM5655AK9A44";
+            string actual = Calculation.GetKODECountry(kode);
+            Assert.IsInstanceOfType(actual, typeof(string));
+        }
+
+        [TestMethod]
+        //Проверяет какой тип данных выдаст
+        public void GetKODE_Type()
+        {
+            string kode = "JHMCM5655AK9A44";
+            bool actual = Calculation.CheckKODE(kode);
+            Assert.IsInstanceOfType(actual, typeof(bool));
+        }
+
+        //Проверяет корректность метода если ввести не существующию географическую зону (Выведет null)
+        [TestMethod]
+        public void GetKODECountry_NULL()
+        {
+            string kode = "JHMCM5655A00A4453";
+            string conutry = "";
+            string actual = Calculation.GetKODECountry(kode);
+            Assert.AreEqual(conutry, actual);
+        }
+
+        //Проверяет корректность метода, если отправить не 17 буквенный kode (Выведет null)
+        [TestMethod]
+        public void GetKODECountry_NULL_Not17()
+        {
+            string kode = "JHMCM5655A00A445";
+            string conutry = "";
+            string actual = Calculation.GetKODECountry(kode);
+            Assert.AreEqual(conutry, actual);
+        }
+        //Проверяет корректность метода, если отправить пустой kode (Выведет null)
+        [TestMethod]
+        public void GetKODECountry_NULL_Kode_Pysto()
+        {
+            string kode = "";
+            string conutry = "";
+            string actual = Calculation.GetKODECountry(kode);
+            Assert.AreEqual(conutry, actual);
+        }
+    }
+}
---
+++ WSUniversalLib/Calculation.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSUniversalLib
+
+{
+    public class Calculation
+    {
+
+        public static int GetPriorityForAgent(int priority)
+        {
+            int agentType = 0;
+            float experience = 0;
+
+            Random x = new Random();
+
+            int age = x.Next(18, 100);
+
+            if ((age >= 20) && (age <= 29))
+            {
+                experience = Convert.ToSingle(age + (age * 0.1));
+                agentType = 1;
+                priority = 4;
+                return priority;
+            }
+            else if ((age >= 30) && (age <= 39))
+            {
+                experience = Convert.ToSingle(age + (age * 0.4));
+                priority = 3;
+                agentType = 2;
+                return priority;
+            }
+            else if ((age >= 40) && (age <= 49))
+            {
+                experience = Convert.ToSingle(age + (age * 0.7));
+                priority = 2;
+                agentType = 3;
+                return priority;
+            }
+            else if ((age >= 50) && (age <= 59))
+            {
+                experience = Convert.ToSingle(age + (age * 0.9));
+                priority = 1;
+                agentType = 4;
+                return priority;
+            }
+            else
+            {
+                return age;
+            }
+        }
+
+
+        public static List<char> vs = new List<char>() { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+
+        public static bool CheckKODE(string kode)
+        {
+
+            int kolvo = 0;
+            if (kode.Length == 17)
+            {
+                for (int i = 0; i < kode.Length; i++)
+                {
+
+                    for (int j = 0; j < vs.Count; j++)
+                    {
+                        if (kode[i] == vs[j])
+                        {
+                            kolvo++;
+
+                        }
+                    }
+                }
+                if (kolvo == 17)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static string GetKODECountry(string kode)
+        {
+            if (CheckKODE(kode))
+            {
+                for (int i = 10; i <= 17; i++)
+                {
+                    if (kode[11] == vs[i])
+                    {
+                        return "Африка";
+                    }
+                }
+                for (int i = 18; i <= 24; i++)
+                {
+                    if (kode[11] == vs[i])
+                    {
+                        return "Азия";
+                    }
+
+                }
+                for (int i = 25; i <= 32; i++)
+                {
+                    if (kode[11] == vs[i])
+                    {
+                        return "Европа";
+                    }
+                }
+                for (int i = 1; i <= 5; i++)
+                {
+                    if (kode[11] == vs[i])
+                    {
+                        return "Северная Америка";
+                    }
+                }
+                for (int i = 6; i <= 7; i++)
+                {
+                    if (kode[11] == vs[i])
+                    {
+                        return "Океания";
+                    }
+                }
+                for (int i = 8; i <= 9; i++)
+                {
+                    if (kode[11] == vs[i])
+                    {
+                        return "Южная Америка";
+                    }
+                }
+            }
+
+            return "";
+        }
+    }
+}
+
+
